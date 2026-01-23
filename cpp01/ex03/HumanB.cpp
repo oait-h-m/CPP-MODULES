4314e91a3627ecbf63018ff0b9aa7702150faeac
oait-h-m <oussamaaithmed1@gmail.com>
2026-01-23 15:53:51 +0100

    add HumanB.cpp

---
+++ cpp01/ex03/HumanB.cpp
@@ -0,0 +1,22 @@
+#include "Weapon.hpp"
+#include "HumanB.hpp"
+#include <iostream>
+
+HumanB::HumanB(const std::string name)
+{
+    this->name = name;
+    weapon = NULL;
+}
+
+void    HumanB::setWeapon(Weapon& w)
+{
+    weapon = &w;
+}
+
+void    HumanB::attack()
+{
+    if (weapon)
+        std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+    else
+        std::cout << name << " has no weapon" << std::endl;
+}
