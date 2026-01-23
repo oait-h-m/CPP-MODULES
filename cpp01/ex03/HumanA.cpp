94cc97b2519a8e82356775cf86f49fba3ef6357f
oait-h-m <oussamaaithmed1@gmail.com>
2026-01-23 15:52:25 +0100

    add HumanA.cpp

---
+++ cpp01/ex03/HumanA.cpp
@@ -0,0 +1,11 @@
+#include "HumanA.hpp"
+
+HumanA::HumanA(const std::string& name, Weapon& w) : weapon(w)
+{
+    this->name = name;
+}
+
+void    HumanA::attack()
+{
+    std::cout << name << " attacks with their " << weapon.getType() << std::endl;
+}
