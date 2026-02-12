7242a8a6b4731ba483289ec8e3ab6b80e2507aad
oait-h-m <oussamaaithmed1@gmail.com>
2026-02-12 12:24:14 +0100

    add Cat.cpp

---
+++ cpp04/ex00/Cat.cpp
@@ -0,0 +1,34 @@
+#include "Cat.hpp"
+
+Cat::Cat() : Animal()
+{
+	type = "Cat";
+	std::cout << "Cat constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat &other) : Animal(other)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+}
+
+Cat	&Cat::operator=(const Cat &other)
+{
+	if (this != &other)
+		Animal::operator=(other);
+	return *this;
+}
+
+Cat::~Cat() 
+{
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+std::string	Cat::getType() const
+{
+	return type;
+}
+
+void	Cat::makeSound() const
+{
+	std::cout << "A Cat makes sound: Meow!" << std::endl;
+}
