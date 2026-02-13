ed2a29f7087a0bbed77bad69ba96f68a794cd11f
oait-h-m <oussamaaithmed1@gmail.com>
2026-02-13 15:24:20 +0100

    finished ex02

---
+++ cpp04/ex02/Dog.cpp
@@ -0,0 +1,42 @@
+#include "Dog.hpp"
+
+Dog::Dog() : Animal()
+{
+    type = "Dog";
+    brain = new Brain();  
+    std::cout << "Dog constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog &other) : Animal(other)
+{
+    brain = new Brain(*other.brain);  
+    std::cout << "Dog copy constructor called" << std::endl;
+}
+
+Dog &Dog::operator=(const Dog &other)
+{
+    std::cout << "Dog copy assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        Animal::operator=(other);
+        delete brain;  
+        brain = new Brain(*other.brain); 
+    }
+    return *this;
+}
+
+Dog::~Dog()
+{
+    delete brain;  
+    std::cout << "Dog destructor called" << std::endl;
+}
+
+void Dog::makeSound() const
+{
+    std::cout << "A Dog makes sound: bark!" << std::endl;
+}
+
+Brain* Dog::getBrain() const
+{
+    return brain;
+}
