d1a52d3b5fc6e9f50659411092be9d12fd2e42b8
oait-h-m <oussamaaithmed1@gmail.com>
2026-02-28 06:15:00 +0000

    add AForm.hpp

---
+++ cpp05/ex03/AForm.hpp
@@ -0,0 +1,50 @@
+#ifndef AFORM_HPP
+#define AFORM_HPP
+
+#include <iostream>
+
+class Bureaucrat ;
+class AForm
+{
+private:
+	const std::string	name;
+	bool			isSigned;
+	const int		gradeToSign;
+	const int		gradeToExecute;
+protected:
+    virtual void executeAction() const = 0;
+public:
+	AForm(const std::string &name, int gradeToSign, int gradeToExecute);
+	AForm(const AForm &other);
+	AForm &operator=(const AForm &other);
+	virtual	~AForm();
+
+	std::string	getName() const;
+	bool		getIsSigned() const;
+	int		getGradeToSign() const;
+	int		getGradeToExecute() const;
+
+	void		beSigned(const Bureaucrat &bureaucrat);
+	virtual void		execute(Bureaucrat const & executor) const;
+
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			const char* what() const throw();
+	};
+	class FormNotSignedException : public std::exception
+	{
+		public:
+			const char* what() const throw();
+    };
+};
+
+std::ostream	&operator<<(std::ostream &out, const AForm &obj);
+
+#endif
