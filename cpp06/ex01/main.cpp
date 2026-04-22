d23191b006371df09b6133a9499b6f17ad0ad423
Oussama Ait H'mad <oussamaaithmed1@gmail.com>
2026-04-22 11:29:30 +0100

    finish

---
+++ cpp06/ex01/main.cpp
@@ -0,0 +1,23 @@
+#include "Serializer.hpp"
+#include "Data.hpp"
+
+int main()
+{
+    Data data;
+
+    data.name = "Oussama";
+    data.value = 1337;
+
+    Data* original = &data;
+
+    uintptr_t raw = Serializer::serialize(original);
+    Data* restored = Serializer::deserialize(raw);
+
+    std::cout << "Original pointer : " << original << "\n";
+    std::cout << "Serialized value : " << raw << "\n";
+    std::cout << "Restored pointer : " << restored << "\n";
+
+    std::cout << "------------>Data<--------------" << std::endl;
+    std::cout << "name = " << restored->name << std::endl;
+    std::cout << "value = " << restored->value << std::endl;
+}
