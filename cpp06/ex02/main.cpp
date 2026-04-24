550d7997564b5afbd81a18e501284ac67005f753
Oussama Ait H'mad <oussamaaithmed1@gmail.com>
2026-04-24 18:47:58 +0100

    finish

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include "Base.hpp"
+#include "Identify.hpp"
+
+int main()
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        Base* p = generate();
+
+        std::cout << "identify(Base*): ";
+        identify(p);
+
+        std::cout << "identify(Base&): ";
+        identify(*p);
+
+        delete p;
+    }
+    return 0;
+}
