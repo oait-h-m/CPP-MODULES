c51d3ef592d59e057cf1499605ad54f296020342
Oussama Ait H'mad <oussamaaithmed1@gmail.com>
2026-06-11 16:02:11 +0100

    add MutantStack header file

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,24 @@
+#ifndef MutantSTACK_HPP
+#define MutantSTACK_HPP
+
+#include <stack>
+#include <iostream>
+
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : std::stack<T, Container>
+{
+private:
+
+public:
+    MutantStack() : std::stack<T, Container>() {}
+    MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {}
+    MutantStack& operator=(const MutantStack& other)
+    {
+        std::stack<T, Container>::operator=(other);
+        return *this;
+    }
+    ~MutantStack() {}
+
+};
+
+#endif
