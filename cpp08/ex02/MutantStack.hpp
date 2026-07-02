a14989cc0b822f4f5ac312ac9df80deead09cc80
Oussama Ait H'mad <oussamaaithmed1@gmail.com>
2026-07-02 10:58:19 +0100

    add class

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -3,22 +3,37 @@
 
 #include <stack>
 #include <iostream>
+#include <deque>
 #include <vector>
 
 template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
-private:
-
 public:
     MutantStack() : std::stack<T, Container>() {}
     MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {}
-    MutantStack& operator=(const MutantStack& other)
-    {
+    MutantStack& operator=(const MutantStack& other) {
         std::stack<T, Container>::operator=(other);
         return *this;
     }
     ~MutantStack() {}
+
+    typedef typename std::stack<T, Container>::container_type::iterator iterator;
+    typedef typename std::stack<T, Container>::container_type::const_iterator const_iterator;
+
+    iterator begin() {
+        return this->c.begin();
+    }
+    iterator end() {
+        return this->c.end();
+    }
+
+    const_iterator begin() const {
+        return this->c.begin();
+    }
+    const_iterator end() const {
+        return this->c.end();
+    }
 
 };
 
